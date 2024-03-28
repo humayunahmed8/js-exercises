b5762d11bda102cf042739aef72d26135d336c04
humayunahmed8 <humayunfarhad3@gmail.com>
2024-03-28 14:04:11 +0600

    Interpolation & Variable Substitutions

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ let myName = `Humayun`;
 let showMyName = `${myName}`;
 console.log(showMyName);
 
+let myFriendName = `Jhon`;
+let showMyFriendName = `${myFriendName}`;
+console.log(showMyFriendName);
+
