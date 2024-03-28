7a18e14e2d900d0ba9d7d6c1ccfe64508d96dbde
humayunahmed8 <humayunfarhad3@gmail.com>
2024-03-28 14:53:51 +0600

    Expression Substitution

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Interpolation
+// Interpolation & Variable Substitutions
 
 let myName = `Humayun`;
 let showMyName = `${myName}`;
@@ -8,3 +8,11 @@ let myFriendName = `Jhon`;
 let showMyFriendName = `${myFriendName}`;
 console.log(showMyFriendName);
 
+// Expression Substitution
+let price = 50;
+let vat = 50;
+let vatAmount = `${(price * vat) / 100}`; // Calculate the VAT amount using template string
+let subtotal = `${price + parseFloat(vatAmount)}`; // Calculate the subtotal using template string
+
+// console.log(`VAT Amount: ${vatAmount}`);
+console.log(`Subtotal: ${subtotal}`);
