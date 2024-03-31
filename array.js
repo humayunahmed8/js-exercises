b6bc3ba3d448560b61d5c518266146056067ad03
humayunahmed8 <humayunfarhad3@gmail.com>
2024-03-31 12:56:11 +0600

    exercise again array methods

---
+++ array.js
@@ -0,0 +1,77 @@
+const district = ['Feni', 'Cumilla', 'Chattagram', 'Dhaka'];
+
+/* একটি array এর ভেতর কয়টি আইটেম আছে সেটা বের করার জন্য 
+ * array.length 
+ *
+ * 
+*/
+
+console.log(district.length); // 4 
+
+/* একটি array কে string এ কনভার্ট করার জন্য 
+ * array.string() 
+ *
+ * 
+*/
+
+console.log(district.toString()); //Feni,Cumilla,Chattagram,Dhaka
+
+/* একটি array থেকে নির্দিষ্ট key দিয়ে একটি আইটেমকে বের করে নিয়ে আসা 
+ * array.at() 
+ *
+ * 
+*/
+
+console.log(district.at(1)); //Cumilla
+
+/* একটি array এর সবগুলো আইটেম কে string এ কনভার্ট করতে  
+ * array.join()
+ *
+ * এক্ষেত্রে প্রত্যেকটি string এর মাঝখানে যে কোন text যুক্ত করা যেতে পারে
+ * 
+*/
+
+console.log(district.join("*")) // Feni*Cumilla*Chattagram*Dhaka
+
+/* একটি array থেকে নির্দিষ্ট একটি আইটেম remove করে দিতে   
+ * array.pop()
+ *
+ * 
+*/
+
+const removeOneItemFromArray = district.pop(); //Dhaka
+
+console.log(district); //['Feni', 'Cumilla', 'Chattagram'] 
+
+
+/* একটি array এর শেষে নতুন একটি আইটেম add করতে    
+ * array.push()
+ *
+ * 
+*/
+
+const addItemArray = district.push("Rajshahi");
+
+console.log(district); //['Feni', 'Cumilla', 'Chattagram', 'Rajshahi']
+
+/* একটি array এর প্রথম আইটেম remove করতে 
+ * array.shift()
+ *
+ * 
+*/
+
+const removeFirstItemFromArray = district.shift();
+
+console.log(district); // ['Cumilla', 'Chattagram', 'Rajshahi']
+
+
+/* একটি array এর প্রথম আইটেম add করতে 
+ * array.unshift()
+ *
+ * 
+*/
+
+const addFirstItemFromArray = district.unshift("Pabna");
+
+console.log(district); //['Pabna', 'Cumilla', 'Chattagram', 'Rajshahi']
+
