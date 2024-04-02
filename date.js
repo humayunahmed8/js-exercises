213fde093363542cab35d18e8bb93b0f3b586dbe
humayunahmed8 <humayunfarhad3@gmail.com>
2024-04-02 17:22:31 +0600

    01 January 1970 plus miliseconds

--- date.js
+++ date.js
@@ -7,4 +7,19 @@ console.log(d);
 const fullDate = new Date(2018, 11, 24, 10, 33, 30, 0);
 console.log(fullDate);
 
+// Overflow the next year
+
+const overflowDate = new Date(2024, 16, 18);
+
+document.getElementById("clickDate").addEventListener("click", function() {
+  document.getElementById("date").innerHTML = overflowDate; // Sun May 18 2025 00:00:00 GMT+0600 (Bangladesh Standard Time)
+})
+
+// 01 January 1970 plus 100 000 000 000 milliseconds is
+
+const ml = new Date(100000000000);
+
+console.log(ml);
+
+
 
