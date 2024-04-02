c5d546b42611e056ed45eae5dc7a14c8918c1928
humayunahmed8 <humayunfarhad3@gmail.com>
2024-04-02 16:42:01 +0600

    specified the full with time

--- date.js
+++ date.js
@@ -1,5 +1,10 @@
 // Date 
 const d = new Date();
-
 console.log(d);
+
+// Full Date 
+// 7 numbers specify year, month, day, hour, minute, second, and millisecond (in that order)
+const fullDate = new Date(2018, 11, 24, 10, 33, 30, 0);
+console.log(fullDate);
+
 
