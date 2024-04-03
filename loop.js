5a074eef9107878d9a62687e9608f838c90312ca
humayunahmed8 <humayunfarhad3@gmail.com>
2024-04-03 16:21:57 +0600

    for in loop with an object

--- loop.js
+++ loop.js
@@ -8,3 +8,17 @@ for(let i = 0; i < books.length; i++) {
 
 document.getElementById("loop").innerHTML = text;
 
+// For in Loop with an Object 
+
+let plainText = "";
+const bookList = {
+  book1: { title: "Book 1", author: "Author 1" },
+  book2: { title: "Book 2", author: "Author 2" },
+  book3: { title: "Book 3", author: "Author 3" }
+};
+
+for(let key in bookList) {
+  plainText += "Title: " + bookList[key].title + ", Author: " + bookList[key].author + "<br>";
+}
+
+document.getElementById("loopTwo").innerHTML = plainText;
