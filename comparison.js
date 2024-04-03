12dac5fca868e1952f5294be6bad274480a03daa
humayunahmed8 <humayunfarhad3@gmail.com>
2024-04-03 13:03:50 +0600

    JS Comparison

---
+++ comparison.js
@@ -0,0 +1,69 @@
+// JS Comparison 
+
+if(5 > 4) {
+  console.log("Right"); // Right
+} else {
+  console.log("Wrong");
+}
+
+
+if(5 == 4) {
+  console.log("Right"); 
+} else {
+  console.log("Wrong"); // Wrong
+}
+
+if(4 == 4) {
+  console.log("Right"); // Right
+} else {
+  console.log("Wrong"); 
+}
+
+if(false == false) {
+  console.log("Right"); // Right
+} else {
+  console.log("Wrong"); 
+}
+
+if('hello' == 'hello') {
+  console.log("Right");  // Right
+} else {
+  console.log("Wrong"); 
+}
+
+
+if('hello' === 'hello') {
+  console.log("Right");  // Right
+} else {
+  console.log("Wrong"); 
+}
+
+if(5 != 4) {
+  console.log("Right"); // Right
+}else {
+  console.log("Wrong");
+}
+
+if(5 != "4") {
+  console.log("Right"); // Right
+}else {
+  console.log("Wrong");
+}
+
+if(5 !== "4") {
+  console.log("Right"); // Right
+}else {
+  console.log("Wrong"); 
+}
+
+if(10 >= 4) {
+  console.log("Right"); // Right
+}else {
+  console.log("Wrong");
+}
+
+if(3 >= 4) {
+  console.log("Right"); 
+}else {
+  console.log("Wrong"); // Wrong
+}
