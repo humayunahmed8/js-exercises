b2031b272fe79a01d1d64626ae9e5b62b3503974
humayunahmed8 <humayunfarhad3@gmail.com>
2024-04-04 11:58:43 +0600

    while loop

--- loop.js
+++ loop.js
@@ -55,3 +55,10 @@ for(x of numbList) {
   theText += x + "<br>";
 }
 document.getElementById("numbListTwo").innerHTML = theText;
+
+//While loop
+let i = 0;
+while(i < 10) {
+  console.log(i);
+  i++;
+}
