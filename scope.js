dcdc90e7f8c1d4c91ba7268e6b44ed37d6c05e11
humayunahmed8 <developer.humayun@gmail.com>
2024-04-15 19:46:47 +0600

    JS Scope

---
+++ scope.js
@@ -0,0 +1,12 @@
+// let x = 1;
+// if(x < 2) {
+//   let y = 5;
+//   console.log(y);
+// }
+// console.log(y);
+
+function myFunction() {
+  const carName = "Volvo";   // Function Scope
+}
+console.log(carName);
+
