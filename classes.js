09bc61dd9c2f5ec890524245ef6631a2114857bf
humayunahmed8 <developer.humayun@gmail.com>
2024-04-22 16:10:41 +0600

    JS classes exercise

--- classes.js
+++ classes.js
@@ -1,9 +1,16 @@
 class Person {
-  constructor(name, age) {
+  constructor(name, year) {
     this.name = name;
-    this.age = age;
+    this.year = year;
+  }
+  age() {
+    const date = new Date();
+    return date.getFullYear() - this.year;
   }
 }
 
-const person1 = new Person("Sakib", 35);
-console.log(person1.name);
+const person1 = new Person("Sakib", 2000);
+const person2 = new Person("Tamim", 1999);
+
+console.log(`My name is ${person1.name}, I am ${person1.age()} old`);
+console.log(`My name is ${person2.name}, I am ${person2.age()} old`);
