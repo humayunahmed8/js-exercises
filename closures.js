4702093a111fe71df63f23016a9bbc3568621e93
humayunahmed8 <developer.humayun@gmail.com>
2024-05-07 19:06:50 +0600

    Counter Dilemma

--- closures.js
+++ closures.js
@@ -19,3 +19,19 @@ function localScope() {
 
 console.log(localScope());
 
+
+// Counter Dilemma
+
+let counter = 0;
+
+function add() {
+  // counter = counter + 1
+  counter += 1;
+}
+
+add();
+add();
+add();
+
+
+console.log(counter);
