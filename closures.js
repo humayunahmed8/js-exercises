b27fe204822d6d4cceb9c38aadd3029bd21b8b46
humayunahmed8 <developer.humayun@gmail.com>
2024-05-07 19:18:42 +0600

    Closures

--- closures.js
+++ closures.js
@@ -35,3 +35,17 @@ add();
 
 
 console.log(counter);
+
+
+// Closure 
+
+const state = (function() {
+  let counter = 0;
+  return function() {
+    counter += 1;
+    return counter;
+  }
+})();
+
+console.log(state());
+console.log(state())
